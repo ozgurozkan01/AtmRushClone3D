354d1e6fbc798c11c8dba4103d5b3ceb5cbda286
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2022-10-08 20:35:49 +0300

    add player rotation controller

--- Assets/Scripts/Player/PlayerRotationController.cs
+++ Assets/Scripts/Player/PlayerRotationController.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerRotationController : MonoBehaviour
 {
-    /*public void PlayerRotatingOppositeDirection()
+    public void PlayerRotatingOppositeDirection()
     {
         transform.rotation = new Quaternion(0f, 180f, 0f, 0f);
-    }*/
+    }
 }
